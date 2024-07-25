9ab5d37e122cd8ac2b091067520f1f9c4436d572
Roman Ratushnyi <remenok9@gmail.com>
2024-07-25 20:41:02 +0300

    Add park officer model

---
+++ server/models/ParkOfficer.js
@@ -0,0 +1,42 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ParkOfficer extends Model {
+    static associate(models) {
+
+    }
+  }
+  ParkOfficer.init({
+    fullName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      }
+    },
+    badgeNumber: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      }
+    },
+    district: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'ParkOfficer',
+    tableName: 'park_officers',
+    underscored: true
+  });
+
+  return ParkOfficer;
+};
