21698a0e078d6338e805d3a517472fcc8a18f628
Roman Ratushnyi <remenok9@gmail.com>
2024-07-30 18:29:19 +0300

    Minor changes in http tests

--- server/routes/protocol.js
+++ server/routes/protocol.js
@@ -13,12 +13,11 @@ const ProtocolController = require('../controllers/ProtocolController');
 
 protocolRouter
 .route('/')
-.get(paginate, ProtocolController.getAllProtocols)
+.get(paginate, ProtocolController.getAllProtocolsByOfficerID)
 .post(uploadImages, ProtocolController.createProtocol);
 
 protocolRouter
 .route('/:id')
-.get(paginate, ProtocolController.getAllProtocolsByOfficerID)
 .put(uploadImages, ProtocolController.updateProtocolByID)
 .delete(ProtocolController.deleteProtocolByID);
 
