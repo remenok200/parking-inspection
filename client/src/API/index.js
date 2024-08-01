cb7769948439fcb27050fbe3fc9b78817f99284e
Roman Ratushnyi <remenok9@gmail.com>
2024-08-01 19:49:13 +0300

    Add officer (front) - done

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -9,3 +9,5 @@ export const getParkOfficers = async () => await httpClient.get('/parkOfficers')
 export const deleteParkOfficer = async (parkOfficerID) => await httpClient.delete(`/parkOfficers/${parkOfficerID}`);
 
 export const dismissParkOfficer = async (parkOfficerID) => await httpClient.put(`/parkOfficers/${parkOfficerID}/dismiss`);
+
+export const addParkOfficer = async (parkOfficer) => await httpClient.post('/parkOfficers', parkOfficer);
--- client/src/redux/slices/parkOfficerSlice.js
+++ client/src/redux/slices/parkOfficerSlice.js
@@ -3,37 +3,59 @@ import * as API from '../../API';
 
 const SLICE_NAME = 'parkOfficer';
 
-const getParkOfficers = createAsyncThunk(`${SLICE_NAME}/getParkOfficers`, async (param, thunkAPI) => {
-  try {
-    const { data: { data: parkOfficers } } = await API.getParkOfficers();
+const getParkOfficers = createAsyncThunk(
+  `${SLICE_NAME}/getParkOfficers`,
+  async (param, thunkAPI) => {
+    try {
+      const {
+        data: { data: parkOfficers },
+      } = await API.getParkOfficers();
 
-    return parkOfficers;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
+      return parkOfficers;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
   }
-});
+);
 
-const deleteParkOfficer = createAsyncThunk(`${SLICE_NAME}/deleteParkOfficer`, async (parkOfficerID, thunkAPI) => {
-  try {
-    await API.deleteParkOfficer(parkOfficerID);
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
+const deleteParkOfficer = createAsyncThunk(
+  `${SLICE_NAME}/deleteParkOfficer`,
+  async (parkOfficerID, thunkAPI) => {
+    try {
+      await API.deleteParkOfficer(parkOfficerID);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
   }
-});
+);
 
-const dismissParkOfficer = createAsyncThunk(`${SLICE_NAME}/dismissParkOfficer`, async (parkOfficerID, thunkAPI) => {
-  try {
-    await API.dismissParkOfficer(parkOfficerID);
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
+const dismissParkOfficer = createAsyncThunk(
+  `${SLICE_NAME}/dismissParkOfficer`,
+  async (parkOfficerID, thunkAPI) => {
+    try {
+      await API.dismissParkOfficer(parkOfficerID);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
   }
-});
+);
+
+const addParkOfficer = createAsyncThunk(
+  `${SLICE_NAME}`,
+  async (parkOfficer, thunkAPI) => {
+    try {
+      await API.addParkOfficer(parkOfficer);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
 const initialState = {
   parkOfficers: [],
   isLoading: false,
-  error: null
-}
+  error: null,
+};
 
 const parkOfficerSlice = createSlice({
   name: SLICE_NAME,
@@ -84,11 +106,26 @@ const parkOfficerSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
     });
-  }
+
+    builder.addCase(addParkOfficer.pending, (state, action) => {
+      state.error = null;
+      state.isLoading = true;
+    });
+
+    builder.addCase(addParkOfficer.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.error = null;
+    });
+
+    builder.addCase(addParkOfficer.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    });
+  },
 });
 
 const { reducer } = parkOfficerSlice;
 
-export { getParkOfficers, deleteParkOfficer, dismissParkOfficer };
+export { getParkOfficers, deleteParkOfficer, dismissParkOfficer, addParkOfficer };
 
 export default reducer;
