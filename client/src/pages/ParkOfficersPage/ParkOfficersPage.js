ad7a31216a65ae3717018af09697c9cb9754edee
Roman Ratushnyi <remenok9@gmail.com>
2024-08-05 15:11:38 +0300

    Add filter to park officers page

--- client/src/pages/ParkOfficersPage/ParkOfficersPage.js
+++ client/src/pages/ParkOfficersPage/ParkOfficersPage.js
@@ -3,9 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getParkOfficers } from '../../redux/slices/parkOfficerSlice';
 import ParkOfficer from '../../components/ParkOfficer/ParkOfficer';
 import AddParkOfficer from '../../components/Modals/AddParkOfficer';
+import styles from './ParkOfficersPage.module.scss';
 
 const ParkOfficersPage = () => {
   const [addParkOfficerModalOpen, setAddParkOfficerModalOpen] = useState(false);
+  const [filter, setFilter] = useState('worked');
 
   const { parkOfficers, isLoading, error } = useSelector(
     (state) => state.parkOfficers
@@ -26,10 +28,17 @@ const ParkOfficersPage = () => {
   }
 
   const filteredParkOfficers = parkOfficers.filter(
-    ({ fullName, badgeNumber, district }) =>
-      fullName.toLowerCase().includes(searchValue.toLowerCase()) ||
-      badgeNumber.toLowerCase().includes(searchValue.toLowerCase()) ||
-      district.toLowerCase().includes(searchValue.toLowerCase())
+    ({ fullName, badgeNumber, district, isWorked }) => {
+      const matchesSearch =
+        fullName.toLowerCase().includes(searchValue.toLowerCase()) ||
+        badgeNumber.toLowerCase().includes(searchValue.toLowerCase()) ||
+        district.toLowerCase().includes(searchValue.toLowerCase());
+
+      if (filter === 'all') return matchesSearch;
+      if (filter === 'worked') return matchesSearch && isWorked;
+      if (filter === 'notWorked') return matchesSearch && !isWorked;
+      return matchesSearch;
+    }
   );
 
   const parkOfficersCards = filteredParkOfficers.map((currentParkOfficer) => (
@@ -38,14 +47,28 @@ const ParkOfficersPage = () => {
 
   return (
     <section>
-      <input
-        type="text"
-        value={searchValue}
-        onChange={({ target: { value } }) => setSearchValue(value)}
-        placeholder="Search...."
-      />
+      <div className={styles['flex-wrapper']}>
+        <input
+          type="text"
+          value={searchValue}
+          onChange={({ target: { value } }) => setSearchValue(value)}
+          placeholder="Search...."
+        />
 
-      <button onClick={() => setAddParkOfficerModalOpen(true)}>Add officer</button>
+        <button onClick={() => setAddParkOfficerModalOpen(true)}>
+          Add officer
+        </button>
+
+        <select
+          value={filter}
+          onChange={({ target: { value } }) => setFilter(value)}
+          className={styles['filter-select']}
+        >
+          <option value="all">All Officers</option>
+          <option value="worked">Working Officers</option>
+          <option value="notWorked">Not Working Officers</option>
+        </select>
+      </div>
 
       {parkOfficersCards}
 
