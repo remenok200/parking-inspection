bbf58a687f2949e37b154227321caaef8e301764
Roman Ratushnyi <remenok9@gmail.com>
2024-08-07 19:34:12 +0300

    Refactor userErrorToast

--- client/src/hooks/userErrorToast.js
+++ client/src/hooks/userErrorToast.js
@@ -3,27 +3,19 @@ import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 
 const useErrorToast = () => {
-  const usersError = useSelector((state) => state.users.error);
-  const protocolsError = useSelector((state) => state.protocols.error);
-  const parkOfficersError = useSelector((state) => state.parkOfficers.error);
+  const errors = [
+    useSelector((state) => state.users.error),
+    useSelector((state) => state.protocols.error),
+    useSelector((state) => state.parkOfficers.error),
+  ];
 
   useEffect(() => {
-    if (usersError) {
-      toast.error(usersError);
-    }
-  }, [usersError]);
-
-  useEffect(() => {
-    if (protocolsError) {
-      toast.error(protocolsError);
-    }
-  }, [protocolsError]);
-
-  useEffect(() => {
-    if (parkOfficersError) {
-      toast.error(parkOfficersError);
-    }
-  }, [parkOfficersError]);
+    errors.forEach((error) => {
+      if (error) {
+        toast.error(error);
+      }
+    });
+  }, [errors]);
 };
 
 export default useErrorToast;
