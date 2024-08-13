3619c16ce8e4bcac101d46e490ad680acf639ba3
Roman Ratushnyi <remenok9@gmail.com>
2024-08-13 15:22:00 +0300

    Add a delay for the hamburger

--- client/src/components/AdminSidebar/AdminSidebar.js
+++ client/src/components/AdminSidebar/AdminSidebar.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import styles from './AdminSidebar.module.scss';
 
 const AdminSidebar = ({ isOpen, toggleSidebar, setActivePage }) => {
+  const sidebarRef = useRef(null);
+  const [showHamburger, setShowHamburger] = useState(!isOpen);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        if (isOpen) {
+          toggleSidebar(); // Закрытие сайдбара при клике вне его области
+        }
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, toggleSidebar]);
+
+  useEffect(() => {
+    if (isOpen) {
+      setShowHamburger(false);
+    } else {
+      // Добавляем задержку перед показом кнопки гамбургера
+      const timer = setTimeout(() => setShowHamburger(true), 300); // Задержка соответствует времени анимации
+      return () => clearTimeout(timer);
+    }
+  }, [isOpen]);
+
   return (
     <>
       <div
+        ref={sidebarRef}
         className={`${styles.sidebar} ${isOpen ? styles.open : styles.closed}`}
       >
         <button className={styles['toggle-btn']} onClick={toggleSidebar}>
@@ -26,7 +56,7 @@ const AdminSidebar = ({ isOpen, toggleSidebar, setActivePage }) => {
           </ul>
         )}
       </div>
-      {!isOpen && (
+      {showHamburger && (
         <button className={styles['sidebar-open-btn']} onClick={toggleSidebar}>
           <MenuIcon />
         </button>
