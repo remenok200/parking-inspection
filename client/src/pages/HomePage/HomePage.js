43e2e93dda5b083988926152b22d41a72825dc25
Roman Ratushnyi <remenok9@gmail.com>
2024-08-20 18:47:21 +0300

    Implemented a redirect from the home page

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SignIn from '../../components/SignIn/SignIn';
 import SignUp from '../../components/SignUp/SignUp';
 import styles from './HomePage.module.scss';
 
 const HomePage = () => {
   const [state, setState] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const refreshToken = localStorage.getItem('refreshToken');
+    if (refreshToken) {
+      navigate('/officers');
+    }
+  }, []);
 
   const buttonHandler = () => {
     setState((state) => !state);
@@ -15,7 +24,9 @@ const HomePage = () => {
   return (
     <>
       <header>
-        <button onClick={buttonHandler} className={styles.button}>{textButton}</button>
+        <button onClick={buttonHandler} className={styles.button}>
+          {textButton}
+        </button>
       </header>
       <main>{state ? <SignIn /> : <SignUp />}</main>
     </>
