030768873737b0e860a20941198f4346f17fad35
Roman Ratushnyi <remenok9@gmail.com>
2024-08-20 18:59:42 +0300

    Add admin router

---
+++ server/routes/admin.js
@@ -0,0 +1,41 @@
+const adminRouter = require('express').Router();
+
+const AdminController = require('../controllers/AdminController');
+
+const { checkToken } = require('../middlewares/checkToken');
+const { checkAdmin } = require('../middlewares/checkAdmin');
+const { checkBan } = require('../middlewares/checkBan');
+
+adminRouter
+.route('/banlist')
+.post(checkToken, checkBan, checkAdmin, AdminController.ban);
+
+adminRouter
+.route('/banlist/unban/:userId')
+.delete(checkToken, checkBan, checkAdmin, AdminController.unban);
+
+adminRouter
+.route('/all')
+.get(checkToken, checkBan, checkAdmin, AdminController.getAllUsers);
+
+adminRouter
+.route('/all/banned')
+.get(checkToken, checkBan, checkAdmin, AdminController.getAllBannedUsers);
+
+adminRouter
+.route('/tokens/:userId')
+.get(checkToken, checkBan, checkAdmin, AdminController.getUserRefreshTokens);
+
+adminRouter
+.route('/tokens/:tokenId/revoke')
+.put(checkToken, checkBan, checkAdmin, AdminController.revokeRefreshToken);
+
+adminRouter
+.route('/logs/all')
+.get(checkToken, checkBan, checkAdmin, AdminController.getAllLogs);
+
+adminRouter
+.route('/logs/:userId/all')
+.get(checkToken, checkBan, checkAdmin, AdminController.getAllLogsByUserID);
+
+module.exports = adminRouter;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,7 @@
 const userRouter = require('express').Router();
-
 const UserController = require('../controllers/UserController');
-const AdminController = require('../controllers/AdminController');
-
+const adminRouter = require('./admin');
 const { hashPass } = require('../middlewares/hashPassword');
-const { checkToken } = require('../middlewares/checkToken');
-const { checkAdmin } = require('../middlewares/checkAdmin');
-const { checkBan } = require('../middlewares/checkBan');
 
 userRouter
 .route('/sign-up')
@@ -24,36 +19,6 @@ userRouter
 .route('/refresh')
 .post(UserController.refreshSession);
 
-userRouter
-.route('/banlist')
-.post(checkToken, checkBan, checkAdmin, AdminController.ban);
-
-userRouter
-.route('/banlist/unban/:userId')
-.delete(checkToken, checkBan, checkAdmin, AdminController.unban);
-
-userRouter
-.route('/all')
-.get(checkToken, checkBan, checkAdmin, AdminController.getAllUsers);
-
-userRouter
-.route('/all/banned')
-.get(checkToken, checkBan, checkAdmin, AdminController.getAllBannedUsers);
-
-userRouter
-.route('/tokens/:userId')
-.get(checkToken, checkBan, checkAdmin, AdminController.getUserRefreshTokens);
-
-userRouter
-.route('/tokens/:tokenId/revoke')
-.put(checkToken, checkBan, checkAdmin, AdminController.revokeRefreshToken);
-
-userRouter
-.route('/logs/all')
-.get(checkToken, checkBan, checkAdmin, AdminController.getAllLogs);
-
-userRouter
-.route('/logs/:userId/all')
-.get(checkToken, checkBan, checkAdmin, AdminController.getAllLogsByUserID);
+userRouter.use('/', adminRouter); 
 
 module.exports = userRouter;
